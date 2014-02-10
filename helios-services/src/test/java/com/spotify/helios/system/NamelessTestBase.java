201ae7200bf12d9583fa3ecbd8c4003bedc122a4
Daniel Norberg <dano@spotify.com>
2014-02-10 16:32:41 -0500

    nameless test mutex

--- helios-services/src/test/java/com/spotify/helios/system/NamelessTestBase.java
+++ helios-services/src/test/java/com/spotify/helios/system/NamelessTestBase.java
@@ -4,19 +4,45 @@
 
 package com.spotify.helios.system;
 
+import com.google.common.base.Throwables;
+
 import com.spotify.nameless.Service;
 
 import org.junit.After;
 import org.junit.Before;
 
-public class NamelessTestBase extends SystemTestBase {
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+public abstract class NamelessTestBase extends SystemTestBase {
 
   com.spotify.nameless.Service nameless;
+  private FileChannel lockFile;
+  private FileLock lock;
 
   @Override
   @Before
   public void setUp() throws Exception {
     super.setUp();
+
+    // TODO (dano): remove this when nameless can be configured with a unique port
+    final Path lockPath = Paths.get("/tmp/helios-test-nameless-lock");
+    try {
+      log.debug("Taking nameless lock: {}", lockPath);
+      lockFile = FileChannel.open(lockPath, CREATE, WRITE);
+      lock = lockFile.lock();
+    } catch (OverlappingFileLockException | IOException e) {
+      log.error("Failed to take nameless lock: {}", lockPath, e);
+      throw Throwables.propagate(e);
+    }
+
     nameless = new Service();
     nameless.start("--no-log-configuration");
   }
@@ -28,6 +54,12 @@ public class NamelessTestBase extends SystemTestBase {
       nameless.stop();
     } catch (Exception e) {
       e.printStackTrace();
+    }
+    if (lock != null)  {
+      lock.release();
+    }
+    if (lockFile != null) {
+      lockFile.close();
     }
     super.teardown();
   }
