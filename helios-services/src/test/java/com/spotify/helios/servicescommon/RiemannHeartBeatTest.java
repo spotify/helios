490935540a7162831f4eb60175ee2af0895ab768
Daniel Norberg <dano@spotify.com>
2014-02-25 23:06:29 -0500

    fix riemann heartbeat test

--- helios-services/src/test/java/com/spotify/helios/servicescommon/RiemannHeartBeatTest.java
+++ helios-services/src/test/java/com/spotify/helios/servicescommon/RiemannHeartBeatTest.java
@@ -1,29 +1,36 @@
 package com.spotify.helios.servicescommon;
 
 import com.aphyr.riemann.Proto.Event;
+import com.spotify.helios.Polling;
 
 import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class RiemannHeartBeatTest {
 
   @Test
   public void test() throws Exception {
-    CapturingRiemannClient client = new CapturingRiemannClient();
+    final CapturingRiemannClient client = new CapturingRiemannClient();
 
     final RiemannFacade facade = new RiemannFacade(client, "HOSTNAME", "SERVICE");
 
-    RiemannHeartBeat hb = new RiemannHeartBeat(TimeUnit.MILLISECONDS, 1, facade);
+    final RiemannHeartBeat hb = new RiemannHeartBeat(TimeUnit.MILLISECONDS, 1, facade);
     hb.start();
-    Thread.sleep(250); // should get something in that time frame
-    List<Event> events = client.getEvents();
-    assertFalse(events.isEmpty());
 
-    Event event = events.get(0);
+    final List<Event> events = Polling.await(10, TimeUnit.SECONDS, new Callable<List<Event>>() {
+      @Override
+      public List<Event> call() throws Exception {
+        final List<Event> events = client.getEvents();
+        return events != null && !events.isEmpty() ? events : null;
+      }
+    });
+
+    final Event event = events.get(0);
     assertEquals("ok", event.getState());
   }
 }
