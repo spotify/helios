376f8b087761af4ff8830c18ade4cfdac3d27467
Drew Csillag <drewc@spotify.com>
2014-02-27 12:02:10 -0500

    [riemann] fixed noop client to be less finicky on build

--- helios-services/src/main/java/com/spotify/helios/servicescommon/NoOpRiemannClient.java
+++ helios-services/src/main/java/com/spotify/helios/servicescommon/NoOpRiemannClient.java
@@ -3,32 +3,17 @@ package com.spotify.helios.servicescommon;
 import com.aphyr.riemann.Proto.Msg;
 import com.aphyr.riemann.client.AbstractRiemannClient;
 import com.aphyr.riemann.client.IPromise;
+import com.aphyr.riemann.client.Promise;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class NoOpRiemannClient extends AbstractRiemannClient {
   private static final Msg OK_MESSAGE = Msg.newBuilder().setOk(true).build();
-  private static final IPromise<Msg> OK_PROMISE = new IPromise<Msg>() {
-    @Override
-    public void deliver(Object arg0) {
-    }
-
-    @Override
-    public Msg deref() throws IOException {
-      return OK_MESSAGE;
-    }
-
-    @Override
-    public Msg deref(long arg0, TimeUnit arg1) throws IOException {
-      return OK_MESSAGE;
-    }
-
-    @Override
-    public Msg deref(long arg0, TimeUnit arg1, Msg arg2) throws IOException {
-      return OK_MESSAGE;
-    }
-  };
+  private static final IPromise<Msg> OK_PROMISE;
+  static {
+    OK_PROMISE = new Promise<Msg>();
+    OK_PROMISE.deliver(OK_MESSAGE);
+  }
 
   @Override
   public IPromise<Msg> aSendMaybeRecvMessage(Msg arg0) {
