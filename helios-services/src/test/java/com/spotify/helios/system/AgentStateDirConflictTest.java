ca66c53050306d235003cd2cbd4ad2e9cde9ad50
Drew Csillag <drewc@spotify.com>
2014-02-28 11:08:22 -0500

    Fixed AgentStateDirConflictTest to be less finicky

--- helios-services/src/test/java/com/spotify/helios/system/AgentStateDirConflictTest.java
+++ helios-services/src/test/java/com/spotify/helios/system/AgentStateDirConflictTest.java
@@ -6,7 +6,10 @@ package com.spotify.helios.system;
 
 import com.google.common.util.concurrent.Service;
 
+import com.spotify.helios.ZooKeeperStandaloneServerManager;
 import com.spotify.helios.agent.AgentMain;
+
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -14,57 +17,52 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class AgentStateDirConflictTest {
-
-  static final Logger log = LoggerFactory.getLogger(AgentStateDirConflictTest.class);
-
-  Thread.UncaughtExceptionHandler dueh;
-
   @Rule
   public ExpectedException exception = ExpectedException.none();
 
-  Path stateDir;
-
-  AgentMain foo;
-  AgentMain bar;
+  private Thread.UncaughtExceptionHandler dueh;
+  private Path stateDir;
+  private AgentMain first;
+  private AgentMain second;
+  private ZooKeeperStandaloneServerManager zk;
 
   @Before
   public void setup() throws Exception {
+    zk = new ZooKeeperStandaloneServerManager();
     dueh = Thread.getDefaultUncaughtExceptionHandler();
 
     Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
       @Override
-      public void uncaughtException(final Thread t, final Throwable e) {
-      }
+      public void uncaughtException(final Thread t, final Throwable e) {}
     });
 
     stateDir = Files.createTempDirectory("helios-agent-conflict-test");
 
-    foo = new AgentMain("-vvvv",
-                        "--no-log-setup",
-                        "--no-http",
-                        "--name=foo",
-                        "--state-dir", stateDir.toString());
+    first = makeAgent("first");
+    second = makeAgent("second");
+  }
 
-    bar = new AgentMain("-vvvv",
+  private AgentMain makeAgent(String name) throws ArgumentParserException {
+    return new AgentMain("-vvvv",
                         "--no-log-setup",
                         "--no-http",
-                        "--name=bar",
+                        "--name=" + name,
+                        "--zk=" + zk.connectString(),
                         "--state-dir", stateDir.toString());
   }
 
   @After
   public void teardown() {
     FileUtils.deleteQuietly(stateDir.toFile());
-    stopQuietly(foo);
-    stopQuietly(bar);
+    stopQuietly(first);
+    stopQuietly(second);
     Thread.setDefaultUncaughtExceptionHandler(dueh);
+    zk.stop();
   }
 
   private void stopQuietly(final Service service) {
@@ -79,8 +77,8 @@ public class AgentStateDirConflictTest {
 
   @Test
   public void test() throws Exception {
-    foo.startAsync().awaitRunning();
+    first.startAsync().awaitRunning();
     exception.expect(IllegalStateException.class);
-    bar.startAsync().awaitRunning();
+    second.startAsync().awaitRunning();
   }
 }
