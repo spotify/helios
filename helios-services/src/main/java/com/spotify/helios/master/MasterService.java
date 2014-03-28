557448fa8bedd62b9ef7872d8efff6a31aa0e4a4
Daniel Norberg <dano@spotify.com>
2014-03-27 23:45:48 -0400

    master: use persistent ephemeral node

--- helios-services/src/main/java/com/spotify/helios/master/MasterService.java
+++ helios-services/src/main/java/com/spotify/helios/master/MasterService.java
@@ -40,6 +40,7 @@ import com.yammer.metrics.core.MetricsRegistry;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.nodes.PersistentEphemeralNode;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.KeeperException;
 import org.eclipse.jetty.server.Server;
@@ -55,7 +56,7 @@ import ch.qos.logback.access.jetty.RequestLogImpl;
 
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.util.concurrent.Futures.getUnchecked;
-import static org.apache.zookeeper.CreateMode.EPHEMERAL;
+import static org.apache.curator.framework.recipes.nodes.PersistentEphemeralNode.Mode.EPHEMERAL;
 
 /**
  * The Helios master service.
@@ -72,6 +73,7 @@ public class MasterService extends AbstractIdleService {
   private final ZooKeeperClient zooKeeperClient;
 
   private ListenableFuture<RegistrationHandle> namelessHandle;
+  private PersistentEphemeralNode upNode;
 
   /**
    * Create a new service instance. Initializes the control interface and the worker.
@@ -179,6 +181,13 @@ public class MasterService extends AbstractIdleService {
       }
       registrar.shutdown();
     }
+    if (upNode != null) {
+      try {
+        upNode.close();
+      } catch (IOException e) {
+        log.warn("Exception on closing up node: {}", e.getMessage());
+      }
+    }
     zooKeeperClient.close();
   }
 
@@ -212,12 +221,9 @@ public class MasterService extends AbstractIdleService {
       client.ensurePath(Paths.statusMasters());
       client.ensurePath(Paths.historyJobs());
 
-      final String upNode = Paths.statusMasterUp(config.getName());
-      client.ensurePath(upNode, true);
-      if (client.stat(upNode) != null) {
-        client.delete(upNode);
-      }
-      client.createWithMode(upNode, EPHEMERAL);
+      final String upPath = Paths.statusMasterUp(config.getName());
+      upNode = client.persistentEphemeralNode(upPath, EPHEMERAL, new byte[]{});
+      upNode.start();
     } catch (KeeperException e) {
       throw new RuntimeException("zookeeper initialization failed", e);
     }
