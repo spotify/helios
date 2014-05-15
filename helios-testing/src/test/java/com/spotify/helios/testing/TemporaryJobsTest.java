923dd69ed6fc1eb3fd85f6f90864e3272815d914
Daniel Norberg <dano@spotify.com>
2014-05-15 16:53:30 -0400

    rule: remove wiggum use

--- helios-testing/src/test/java/com/spotify/helios/testing/TemporaryJobsTest.java
+++ helios-testing/src/test/java/com/spotify/helios/testing/TemporaryJobsTest.java
@@ -1,7 +1,5 @@
 package com.spotify.helios.testing;
 
-import com.google.common.base.Joiner;
-
 import com.spotify.helios.client.HeliosClient;
 import com.spotify.helios.common.descriptors.Job;
 import com.spotify.helios.common.descriptors.JobId;
@@ -11,9 +9,12 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.net.Socket;
 import java.util.Map;
 
+import static com.google.common.base.Charsets.UTF_8;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -44,64 +45,48 @@ public class TemporaryJobsTest extends SystemTestBase {
 
   public static class SimpleTest {
 
-    private static final String SERVICE = "service";
-    private static final String IMAGE_NAME = "registry:80/spotify/wiggum:0.0.1-SNAPSHOT-c387379";
-
     @Rule
     public final TemporaryJobs temporaryJobs = new TemporaryJobs(client, new TestProber());
 
     private TemporaryJob job1;
-    private TemporaryJob job2;
 
     @Before
     public void setup() {
       job1 = temporaryJobs.job()
-          .image(IMAGE_NAME)
-          .port(SERVICE, 4229, false) // TODO (dano): restore wait after opening firewall
-          .registration("wiggum", "hm", SERVICE)
+          .image("ubuntu:12.04")
+          .command("sh", "-c", "apt-get install nmap -qq --force-yes && " +
+                               "while :; do ncat -e /bin/cat -k -l 4711; done")
+          .port("echo", 4711)
           .deploy(testHost);
-
-      job2 = temporaryJobs.job()
-          .imageFromBuild()
-          .host(testHost)
-          .port("service", 4229, false) // TODO (dano): restore wait after opening firewall
-          .registration("wiggum", "hm", SERVICE)
-          .env("FOO_ADDRESS", Joiner.on(',').join(job1.addresses(SERVICE)))
-          .deploy();
-
     }
 
     @Test
     public void testDeployment() throws Exception {
       // Verify that it is possible to deploy additional jobs during test
-      final TemporaryJob job3 = temporaryJobs.job()
-          .image(IMAGE_NAME)
+      temporaryJobs.job()
+          .image("ubuntu:12.04")
           .host(testHost)
           .deploy();
 
       final Map<JobId, Job> jobs = client.jobs().get(15, SECONDS);
-      assertEquals("wrong number of jobs running", 3, jobs.size());
+      assertEquals("wrong number of jobs running", 2, jobs.size());
       for (Job job : jobs.values()) {
-        assertEquals("wrong job running", IMAGE_NAME, job.getImage());
+        assertEquals("wrong job running", "ubuntu:12.04", job.getImage());
       }
 
-      // TODO (dano): restore this after opening firewall on build agents to allow connecting our docker hosts
-//      final MessageBuilder messageBuilder = MessageBuilderFactory
-//          .newBuilder("hm://wiggum/ping", REQUEST)
-//          .setTtlMillis(3000)
-//          .setMethod("GET");
-//
-//      final Integer port = job1.port(testHost, SERVICE);
-//      assertNotNull("null external port", port);
-//      final Client hermesClient = Hermes.newClient(format("tcp://%s:%s/ping",
-//                                                          DOCKER_ADDRESS, port));
-//      final Message message = hermesClient.send(messageBuilder.build()).get(5, SECONDS);
-//
-//      final List<ByteString> payloads = message.getPayloads();
-//      assertEquals("Wrong number of payloads", 1, payloads.size());
-//      assertEquals("Wrong payload", "PONG", payloads.get(0).toStringUtf8());
+      ping(DOCKER_ADDRESS, job1.port(testHost, "echo"));
     }
 
+    private void ping(final String host, final int port) throws Exception {
+      try (final Socket s = new Socket(host, port)) {
+        byte[] ping = "ping".getBytes(UTF_8);
+        s.getOutputStream().write(ping);
+        final byte[] pong = new byte[4];
+        final int n = s.getInputStream().read(pong);
+        assertEquals(4, n);
+        assertArrayEquals(ping, pong);
+      }
+    }
   }
 
   public static class BadTest {
