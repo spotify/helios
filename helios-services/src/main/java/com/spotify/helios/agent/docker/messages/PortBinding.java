f26deecba756f922919b3b8f24ec7493edeaaa18
Daniel Norberg <dano@spotify.com>
2014-06-03 15:18:25 -0400

    rule: test: verify that containers are reachable

--- helios-services/src/main/java/com/spotify/helios/agent/docker/messages/PortBinding.java
+++ helios-services/src/main/java/com/spotify/helios/agent/docker/messages/PortBinding.java
@@ -30,6 +30,17 @@ public class PortBinding {
     this.hostPort = hostPort;
   }
 
+  public static PortBinding of(final String ip, final String port) {
+    final PortBinding binding = new PortBinding();
+    binding.hostIp(ip);
+    binding.hostPort(port);
+    return binding;
+  }
+
+  public static PortBinding of(final String ip, final int port) {
+    return of(ip, String.valueOf(port));
+  }
+
   @Override
   public boolean equals(final Object o) {
     if (this == o) {
