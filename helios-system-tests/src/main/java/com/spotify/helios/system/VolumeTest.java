e59c682f288a7e0d4dc9056bedfd2eef99e53ef0
Daniel Norberg <dano@spotify.com>
2014-06-13 17:24:20 -0400

    make VolumeTest tolerate uml slirp accept/close
    The uml kernel slirp driver will accept and close
    even if there's no nothing bound in uml userland.

--- helios-system-tests/src/main/java/com/spotify/helios/system/VolumeTest.java
+++ helios-system-tests/src/main/java/com/spotify/helios/system/VolumeTest.java
@@ -21,8 +21,6 @@
 
 package com.spotify.helios.system;
 
-import com.google.common.io.ByteStreams;
-
 import com.spotify.helios.Polling;
 import com.spotify.helios.client.HeliosClient;
 import com.spotify.helios.common.descriptors.Deployment;
@@ -34,11 +32,15 @@ import com.spotify.helios.common.protocol.CreateJobResponse;
 import com.spotify.helios.common.protocol.JobDeployResponse;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.util.concurrent.Callable;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.spotify.helios.common.descriptors.Goal.START;
 import static com.spotify.helios.common.descriptors.HostStatus.Status.UP;
 import static com.spotify.helios.common.descriptors.TaskStatus.State.RUNNING;
@@ -48,6 +50,8 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.junit.Assert.assertEquals;
 
 public class VolumeTest extends SystemTestBase {
+
+  private static final Logger log = LoggerFactory.getLogger(VolumeTest.class);
 
   @Test
   public void test() throws Exception {
@@ -63,10 +67,12 @@ public class VolumeTest extends SystemTestBase {
         .setImage(BUSYBOX)
         .addVolume("/volume")
         .addVolume("/urandom", "/dev/urandom")
-        .setCommand(asList("sh", "-c", "echo foo > /volume/bar && " +
-                                       "nc -p 4711 -le dd if=/volume/bar &&" +
-                                       "nc -p 4711 -lle dd if=/urandom bs=1 count=4"))
-        .addPort("random", PortMapping.of(4711))
+        .addVolume("/hostdev", "/dev")
+        .setCommand(asList("sh", "-c", "echo foo > /volume/bar; " +
+                                       "nc -p 4711 -le dd if=/volume/bar;" +
+                                       "nc -p 4712 -lle dd if=/urandom bs=1 count=4"))
+        .addPort("bar", PortMapping.of(4711))
+        .addPort("urandom", PortMapping.of(4712))
         .build();
     final JobId jobId = job.getId();
 
@@ -87,27 +93,48 @@ public class VolumeTest extends SystemTestBase {
     taskStatus = awaitJobState(client, testHost(), jobId, RUNNING, LONG_WAIT_MINUTES, MINUTES);
     assertEquals(job, taskStatus.getJob());
 
-    final Integer port = taskStatus.getPorts().get("random").getExternalPort();
-    assert port != null;
+    final Integer bar = taskStatus.getPorts().get("bar").getExternalPort();
+    final Integer urandom = taskStatus.getPorts().get("urandom").getExternalPort();
+
+    assert bar != null;
+    assert urandom != null;
 
     // Read "foo" from /volume/bar
-    final String foo = Polling.await(LONG_WAIT_MINUTES, MINUTES, new Callable<String>() {
+    final String foo = recvUtf8(bar, 3);
+    assertEquals("foo", foo);
+
+    // Attempt to read some random bytes from the mounted /dev/urandom
+    recv(urandom, 4);
+  }
+
+  private String recvUtf8(final int port, final int n) throws Exception {
+    final byte[] bytes = recv(port, n);
+    return new String(bytes, UTF_8);
+  }
+
+  private byte[] recv(final int port, final int n) throws Exception {
+    checkArgument(n > 0, "n must be > 0");
+    return Polling.await(LONG_WAIT_MINUTES, MINUTES, new Callable<byte[]>() {
       @Override
-      public String call() {
+      public byte[] call() {
         try (final Socket s = new Socket(DOCKER_HOST.address(), port)) {
-          final byte[] foo = new byte[3];
-          ByteStreams.readFully(s.getInputStream(), foo);
-          return new String(foo, UTF_8);
+          final byte[] bytes = new byte[n];
+          final InputStream is = s.getInputStream();
+          final int first = is.read();
+          // Check if the uml kernel slirp driver did an accept->close on us,
+          // i.e. the actual listener is not up yet
+          if (first == -1) {
+            return null;
+          }
+          bytes[0] = (byte) first;
+          for (int i = 1; i < n; i++) {
+            bytes[i] = (byte) is.read();
+          }
+          return bytes;
         } catch (IOException e) {
           return null;
         }
       }
     });
-    assertEquals("foo", foo);
-
-    // Attempt to read some random bytes from the mounted /dev/urandom
-    try (final Socket s = new Socket(DOCKER_HOST.address(), port)) {
-      ByteStreams.readFully(s.getInputStream(), new byte[4]);
-    }
   }
 }
