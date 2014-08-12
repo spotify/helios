77cbcee140345cf415a1e598c8eed512a60af87f
Drew Csillag <drewc@spotify.com>
2014-08-12 12:20:09 -0400

    Allow release version as argument

--- release.sh
+++ release.sh
@@ -6,8 +6,14 @@ then
 	exit 1
 fi
 
+RELEASE_VERSION=$1
+if [[ -n "$RELEASE_VERSION" ]]
+then
+  RELEASE_ARGS="-DreleaseVersion=$RELEASE_VERSION"
+fi
+
 # Use the maven release plugin to update the pom versions and tag the release commit
-mvn -B release:prepare release:clean
+mvn -B release:prepare release:clean $RELEASE_ARGS
 
 echo "Created release tag" `git describe --abbrev=0 master`
 echo "Remember to: ./push-release.sh"
