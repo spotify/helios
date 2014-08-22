3179e2dfce334cf56b335553da067478f0542c66
Ryan Culbertson <ryan@spotify.com>
2014-08-22 18:29:56 -0400

    Refactor build and release process
    Release Helios with just mvn deploy and skip tests.
    Upload debian and jar artifacts by inheriting them from previous builds.

---
+++ create_artifacts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Tar up the staged release, all the pom files, and some build files. We will use these in
+# subsequent build steps to perform the actual release.
+tar -zcvf target/helios-staged-release.tar.gz `find . -name nexus-staging && find . -name pom.xml`
+
+# Copy all debian packages into target/debs, and tar them into target/helios-debs.tar.gz
+mkdir target/debs
+find . -name *.deb -type f -not -path "./target/*" -exec cp {} target/debs/ \;
+tar -C target/debs -zcf target/helios-debs.tar.gz .
+
+# Output build version into file for later Jenkins items
+VERSION="$(egrep -o '<version>.*?\${revision}</version>' pom.xml | sed 's/^<version>\(.*\)${revision}<\/version>$/\1/')${PIPELINE_VERSION}"
+echo ${VERSION} > target/version
--- push-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-git push origin master
-TAGREF=refs/tags/$(git describe --abbrev=0 master)
-git push origin ${TAGREF}:${TAGREF}
