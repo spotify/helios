6d7df5fbabd7b5c0b41dfea62bc828a4d5e0a705
Anton Lindström <carlantonlindstrom@gmail.com>
2014-11-05 20:40:39 +0100

    Disable -e lxc in circle.sh

--- circle.sh
+++ circle.sh
@@ -3,7 +3,7 @@
 case "$1" in
   pre_machine)
     # have docker bind to localhost
-    docker_opts='DOCKER_OPTS="$DOCKER_OPTS -H tcp://127.0.0.1:2375"'
+    docker_opts='DOCKER_OPTS="-s btrfs -H tcp://127.0.0.1:2375"'
     sudo sh -c "echo '$docker_opts' >> /etc/default/docker"
 
     cat /etc/default/docker
