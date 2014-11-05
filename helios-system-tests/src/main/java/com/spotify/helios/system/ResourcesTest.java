1ab55a506da15c36dd2807453a2a8a73759e8a02
Anton Lindström <carlantonlindstrom@gmail.com>
2014-11-05 22:58:08 +0100

    CircleCI: Just run the ResourcesTest

--- helios-system-tests/src/main/java/com/spotify/helios/system/ResourcesTest.java
+++ helios-system-tests/src/main/java/com/spotify/helios/system/ResourcesTest.java
@@ -87,6 +87,9 @@ public class ResourcesTest extends SystemTestBase {
     try (final DockerClient docker = getNewDockerClient()) {
       ContainerConfig containerConfig =
               docker.inspectContainer(taskStatus.getContainerId()).config();
+      System.err.println(docker.info().executionDriver());
+      //org.junit.Assume.assumeTrue(true);
+      System.err.println(docker.info().toString());
 
       assertEquals(MEMORY, containerConfig.memory());
       assertEquals(MEMORY_SWAP, containerConfig.memorySwap());
