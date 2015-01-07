acc93cf601ab489cc495a067a158c646d83451e1
David Xia <dxia@spotify.com>
2015-01-07 18:03:13 -0500

    Show better error message when master returns error
    * Fixes NPE and #333

---
+++ helios-tools/src/test/java/com/spotify/helios/cli/command/JobStatusCommandTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2014 Spotify AB.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.helios.cli.command;
+
+import com.google.common.util.concurrent.Futures;
+
+import com.spotify.helios.client.HeliosClient;
+import com.spotify.helios.common.descriptors.Job;
+import com.spotify.helios.common.descriptors.JobId;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class JobStatusCommandTest {
+
+  private final Namespace options = mock(Namespace.class);
+  private final HeliosClient client = mock(HeliosClient.class);
+  private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+  private final PrintStream out = new PrintStream(baos);
+
+  private JobStatusCommand command;
+
+  @Before
+  public void setUp() {
+    // use a real, dummy Subparser impl to avoid having to mock out every single call
+    final ArgumentParser parser = ArgumentParsers.newArgumentParser("test");
+    final Subparser subparser = parser.addSubparsers().addParser("list");
+    command = new JobStatusCommand(subparser);
+  }
+
+  @Test
+  public void testMasterError() throws Exception {
+    when(client.jobs()).thenReturn(Futures.immediateFuture((Map<JobId, Job>) null));
+
+    final int ret = command.run(options, client, out, false, null);
+
+    assertEquals(1, ret);
+  }
+}
