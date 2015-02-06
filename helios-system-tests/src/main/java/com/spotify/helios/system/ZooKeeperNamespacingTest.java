3b2ef999caa8f23c413a88daa1b416746637a211
Rohan Singh <rohan@spotify.com>
2015-02-06 14:45:48 +0100

    Ensure all ZooKeeper ops are namespace-aware
    A couple of the methods in DefaultZooKeeperClient were using the ZooKeeper
    client directly without taking into account the ZK namespace.
    
    Fixes #322.

--- helios-system-tests/src/main/java/com/spotify/helios/system/ZooKeeperNamespacingTest.java
+++ helios-system-tests/src/main/java/com/spotify/helios/system/ZooKeeperNamespacingTest.java
@@ -21,6 +21,15 @@
 
 package com.spotify.helios.system;
 
+import com.spotify.helios.client.HeliosClient;
+import com.spotify.helios.common.descriptors.Deployment;
+import com.spotify.helios.common.descriptors.Job;
+import com.spotify.helios.common.descriptors.JobId;
+import com.spotify.helios.common.protocol.CreateJobResponse;
+import com.spotify.helios.common.protocol.JobDeleteResponse;
+import com.spotify.helios.common.protocol.JobDeployResponse;
+import com.spotify.helios.common.protocol.JobUndeployResponse;
+
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -31,9 +40,11 @@ import org.junit.Test;
 import java.util.Collections;
 import java.util.List;
 
+import static com.spotify.helios.common.descriptors.Goal.START;
 import static com.spotify.helios.common.descriptors.HostStatus.Status.UP;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ZooKeeperNamespacingTest extends SystemTestBase {
   private static final String NAMESPACE = "testing";
@@ -63,5 +74,44 @@ public class ZooKeeperNamespacingTest extends SystemTestBase {
     assertEquals(2, result.size());
     assertEquals(NAMESPACE, result.get(0));
     assertEquals("zookeeper", result.get(1)); // we'll always find this
+  }
+
+  @Test
+  public void testDeployment() throws Exception {
+    startDefaultMaster("--zk-namespace", NAMESPACE);
+    startDefaultAgent(testHost(), "--zk-namespace", NAMESPACE);
+
+    final HeliosClient client = defaultClient();
+
+    // Create a job
+    final Job job = Job.newBuilder()
+        .setName(testJobName)
+        .setVersion(testJobVersion)
+        .setImage(BUSYBOX)
+        .setCommand(IDLE_COMMAND)
+        .setCreatingUser(TEST_USER)
+        .build();
+    final JobId jobId = job.getId();
+    final CreateJobResponse created = client.createJob(job).get();
+    assertEquals(CreateJobResponse.Status.OK, created.getStatus());
+
+    // Deploy the job on the agent
+    final Deployment deployment = Deployment.of(jobId, START, TEST_USER);
+    final JobDeployResponse deployed = client.deploy(deployment, testHost()).get();
+    assertEquals(JobDeployResponse.Status.OK, deployed.getStatus());
+
+    // Undeploy the job
+    final JobUndeployResponse undeployed = client.undeploy(jobId, testHost()).get();
+    assertEquals(JobUndeployResponse.Status.OK, undeployed.getStatus());
+
+    // Make sure that it is no longer in the desired state
+    final Deployment undeployedJob = client.deployment(testHost(), jobId).get();
+    assertTrue(undeployedJob == null);
+
+    // Wait for the task to disappear
+    awaitTaskGone(client, testHost(), jobId, LONG_WAIT_SECONDS, SECONDS);
+
+    // Verify that the job can be deleted
+    assertEquals(JobDeleteResponse.Status.OK, client.deleteJob(jobId).get().getStatus());
   }
 }
