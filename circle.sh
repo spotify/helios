883e268769197a7cd5f6c682976b6a929ca08cf4
Tom <tge@molindo.at>
2015-07-18 11:13:57 +0200

    set parallelism to 4 so that free testplan works

--- circle.sh
+++ circle.sh
@@ -3,7 +3,7 @@
 case "$1" in
   pre_machine)
     # ensure correct level of parallelism
-    expected_nodes=5
+    expected_nodes=4
     if [ "$CIRCLE_NODE_TOTAL" -ne "$expected_nodes" ]
     then
         echo "Parallelism is set to ${CIRCLE_NODE_TOTAL}x, but we need ${expected_nodes}x."
