3bd64e5706e3b652dd53d156c0d6e81cd2ffc77e
Matt Brown <mattbrown@spotify.com>
2015-10-05 15:13:36 -0400

    add GIT_COMMIT as job metadata if set as env var
    If `GIT_COMMIT` is set as an environment variable at the time
    JobCreateCommand is executed, it's value is added by default to the job
    metadata map.

--- helios-tools/src/main/java/com/spotify/helios/cli/command/JobCreateCommand.java
+++ helios-tools/src/main/java/com/spotify/helios/cli/command/JobCreateCommand.java
@@ -22,6 +22,8 @@
 package com.spotify.helios.cli.command;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -76,6 +78,20 @@ import static net.sourceforge.argparse4j.impl.Arguments.storeTrue;
 public class JobCreateCommand extends ControlCommand {
 
   private static final JobValidator JOB_VALIDATOR = new JobValidator();
+
+  /**
+   * If any of the keys of this map are set as environment variables (i.e. an environment variable
+   * of GIT_COMMIT_ID=abcdef is set when this command is run), then an entry will be added to the
+   * job's metadata map with with key=this-maps-value, value=environment variable value.
+   *
+   * @see #defaultMetadata()
+   */
+  private static final Map<String, String> DEFAULT_METADATA_ENVVARS = ImmutableMap.of(
+      // GIT_COMMIT is set by the Git plugin in Jenkins, so for jobs created in
+      // Jenkins this will automatically set GIT_COMMIT = the sha1 of the
+      // working tree
+      "GIT_COMMIT", "GIT_COMMIT"
+  );
 
   private final Argument fileArg;
   private final Argument templateArg;
@@ -321,6 +337,7 @@ public class JobCreateCommand extends ControlCommand {
     }
 
     final List<String> envList = options.getList(envArg.getDest());
+    // TODO (mbrown): does this mean that env config is only added when there is a CLI flag too?
     if (!envList.isEmpty()) {
       final Map<String, String> env = Maps.newHashMap();
       // Add environmental variables from helios job configuration file
@@ -332,11 +349,14 @@ public class JobCreateCommand extends ControlCommand {
       builder.setEnv(env);
     }
 
+    Map<String, String> metadata = Maps.newHashMap();
+    metadata.putAll(defaultMetadata());
     final List<String> metadataList = options.getList(metadataArg.getDest());
     if (!metadataList.isEmpty()) {
       // TODO (mbrown): values from job conf file (which maybe involves dereferencing env vars?)
-      builder.setMetadata(parseListOfPairs(metadataList, "metadata"));
+      metadata.putAll(parseListOfPairs(metadataList, "metadata"));
     }
+    builder.setMetadata(metadata);
 
     // Parse port mappings
     final List<String> portSpecs = options.getList(portArg.getDest());
@@ -530,6 +550,27 @@ public class JobCreateCommand extends ControlCommand {
       }
       return 1;
     }
+  }
+
+  /**
+   * Metadata to associate with jobs by default. Currently sets some metadata based upon environment
+   * variables set when the CLI command is run.
+   */
+  private Map<String, String> defaultMetadata() {
+
+    final Builder<String, String> builder = ImmutableMap.builder();
+
+    for (final Map.Entry<String, String> entry : DEFAULT_METADATA_ENVVARS.entrySet()) {
+      final String envKey = entry.getKey();
+      final String metadataKey = entry.getValue();
+
+      final String envValue = System.getenv(envKey);
+      if (envValue != null) {
+        builder.put(metadataKey, envValue);
+      }
+    }
+
+    return builder.build();
   }
 
   private static Map<String, String> parseListOfPairs(final List<String> list,
