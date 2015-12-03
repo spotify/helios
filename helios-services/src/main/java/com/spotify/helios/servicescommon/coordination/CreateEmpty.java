df43821b7edcc9ec80a8763e337ad55c98bc092d
Staffan Gimåker <staffan@spotify.com>
2015-12-03 15:37:57 -0500

    Fix race conditions that might lead to inconsistent DG state
    There were two related race conditions that could cause a DG get
    inconsistent state, both triggered by removing the DG. The issue
    manifests itself with a DG that has no config node but does have
    the status and/or tasks paths.
    
    This could happen if a remove happened at the same time as a
    rolling-update was issued or if the host-update thread updated
    the hosts for that DG as the DG was removed. The latter is much
    more likely to happen.

--- helios-services/src/main/java/com/spotify/helios/servicescommon/coordination/CreateEmpty.java
+++ helios-services/src/main/java/com/spotify/helios/servicescommon/coordination/CreateEmpty.java
@@ -19,11 +19,11 @@ package com.spotify.helios.servicescommon.coordination;
 
 import org.apache.curator.framework.api.transaction.CuratorTransaction;
 
-class CreateEmpty implements ZooKeeperOperation {
+public class CreateEmpty implements ZooKeeperOperation {
 
   private final String path;
 
-  CreateEmpty(final String path) {
+  public CreateEmpty(final String path) {
     this.path = path;
   }
 
@@ -37,5 +37,28 @@ class CreateEmpty implements ZooKeeperOperation {
     return "CreateEmpty{" +
            "path='" + path + '\'' +
            '}';
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    final CreateEmpty that = (CreateEmpty) o;
+
+    if (path != null ? !path.equals(that.path) : that.path != null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return path != null ? path.hashCode() : 0;
   }
 }
