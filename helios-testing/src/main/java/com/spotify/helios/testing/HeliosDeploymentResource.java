db97eb95095c06e62b86730dd8222136bd483c4a
David Xia <dxia@spotify.com>
2016-05-05 13:19:44 -0400

    [helios-testing]: Ensure helios-solo agent is UP
    in addition to registering with ZooKeeper.
    
    This should prevent TemporaryJobs failures where `DefaultDeployer.deploy()`
    gets a list of hosts and then tries to deploy to the single solo agent
    only to find it's not UP yet.
    
    This is a race condition where the solo agent finishes the first part of
    `AgentZooKeeperRegistrar.tryToRegister()` where it writes data to ZK's
    /config/host/<hostname>/id which makes it appear in calls to
    `HeliosClient.listHosts()`, but hasn't yet written the UP node
    to ZK's /status/host/<hostname>/up in the
    latter part of `tryToRegister()`.

--- helios-testing/src/main/java/com/spotify/helios/testing/HeliosDeploymentResource.java
+++ helios-testing/src/main/java/com/spotify/helios/testing/HeliosDeploymentResource.java
@@ -21,6 +21,7 @@ import com.google.common.net.HostAndPort;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import com.spotify.helios.client.HeliosClient;
+import com.spotify.helios.common.descriptors.HostStatus;
 
 import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
@@ -78,9 +79,9 @@ public class HeliosDeploymentResource extends ExternalResource {
       }
     });
 
-    // ensure that at least one agent is available in this HeliosDeployment
-    // this doesn't check that the agent is UP but should be sufficient to avoid conditions
-    // like continuing with the test when starting up helios-solo before the agent is registered
+    // Ensure that at least one agent is available and UP in this HeliosDeployment.
+    // This prevents continuing with the test when starting up helios-solo before the agent is
+    // registered.
     final HeliosClient client = client();
     Polling.awaitUnchecked(30, TimeUnit.SECONDS, new Callable<Boolean>() {
       @Override
@@ -97,12 +98,32 @@ public class HeliosDeploymentResource extends ExternalResource {
           return null;
         }
 
-        if (!hosts.isEmpty()) {
-          log.info("Ensured that at least one agent is available in this HeliosDeployment, "
+        if (hosts.isEmpty()) {
+          log.debug("0 agents in {}, will retry", deployment);
+          return null;
+        }
+
+        log.info("Ensured that at least one agent has registered with ZooKeeper in this"
+                 + "HeliosDeployment. Now checking if it is UP.");
+
+        // Check that at least one host is UP (is maintaining a reasonably reliable
+        // connection to ZK) in addition to registering.
+        final ListenableFuture<HostStatus> statusFuture = client.hostStatus(hosts.get(0));
+        final HostStatus hostStatus;
+        try {
+          hostStatus = statusFuture.get(1, TimeUnit.SECONDS);
+        } catch (TimeoutException | InterruptedException e) {
+          log.debug("timed out waiting for hostStatus request on host {} to finish, will retry",
+                    hosts.get(0));
+          return null;
+        }
+
+        if (hostStatus != null && hostStatus.getStatus() == HostStatus.Status.UP) {
+          log.info("Ensured that at least one agent is UP in this HeliosDeployment, "
                    + "continuing with test!");
           return true;
         }
-        log.debug("0 agents in {}, will retry", deployment);
+
         return null;
       }
     });
