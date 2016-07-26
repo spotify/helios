896c4b0082d3d8a4a36b7d46d62dab16a083f96c
Matt Brown <mattbrown@spotify.com>
2016-07-26 12:17:16 -0400

    CLI support for new listHosts endpoints
    pass the name pattern and the labels filters in the request to the
    server/master rather than filter within the CLI itself.

--- helios-tools/src/main/java/com/spotify/helios/cli/command/HostListCommand.java
+++ helios-tools/src/main/java/com/spotify/helios/cli/command/HostListCommand.java
@@ -17,27 +17,35 @@
 
 package com.spotify.helios.cli.command;
 
-import com.google.common.base.Function;
-import com.google.common.base.Joiner;
-import com.google.common.base.Optional;
-import com.google.common.collect.Collections2;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.collect.Ordering.natural;
+import static com.spotify.helios.cli.Output.formatHostname;
+import static com.spotify.helios.cli.Output.humanDuration;
+import static com.spotify.helios.cli.Output.table;
+import static com.spotify.helios.cli.Utils.allAsMap;
+import static com.spotify.helios.common.descriptors.HostStatus.Status.UP;
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+import static net.sourceforge.argparse4j.impl.Arguments.append;
+import static net.sourceforge.argparse4j.impl.Arguments.storeTrue;
 
 import com.spotify.helios.cli.Table;
-import com.spotify.helios.cli.Utils;
 import com.spotify.helios.client.HeliosClient;
 import com.spotify.helios.common.Json;
 import com.spotify.helios.common.descriptors.DockerVersion;
 import com.spotify.helios.common.descriptors.HostInfo;
-import com.spotify.helios.common.descriptors.HostSelector;
 import com.spotify.helios.common.descriptors.HostStatus;
 import com.spotify.helios.common.descriptors.JobId;
 import com.spotify.helios.common.descriptors.TaskStatus;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import net.sourceforge.argparse4j.inf.Argument;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
@@ -52,19 +60,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-
-import static com.google.common.base.Predicates.containsPattern;
-import static com.google.common.base.Strings.isNullOrEmpty;
-import static com.google.common.collect.Ordering.natural;
-import static com.spotify.helios.cli.Output.formatHostname;
-import static com.spotify.helios.cli.Output.humanDuration;
-import static com.spotify.helios.cli.Output.table;
-import static com.spotify.helios.cli.Utils.allAsMap;
-import static com.spotify.helios.common.descriptors.HostStatus.Status.UP;
-import static java.lang.String.format;
-import static java.lang.System.currentTimeMillis;
-import static net.sourceforge.argparse4j.impl.Arguments.append;
-import static net.sourceforge.argparse4j.impl.Arguments.storeTrue;
 
 public class HostListCommand extends ControlCommand {
 
@@ -123,11 +118,21 @@ public class HostListCommand extends ControlCommand {
   int run(final Namespace options, final HeliosClient client, final PrintStream out,
           final boolean json, final BufferedReader stdin)
       throws ExecutionException, InterruptedException {
+
     final String pattern = options.getString(patternArg.getDest());
-    final List<String> hosts = FluentIterable
-        .from(client.listHosts().get())
-        .filter(containsPattern(pattern))
-        .toList();
+    final List<String> labels = options.getList(hostSelectorsArg.getDest());
+
+    final List<String> hosts;
+
+    if (pattern.isEmpty() && labels.isEmpty()) {
+      hosts = client.listHosts().get();
+    } else if (!pattern.isEmpty() && labels.isEmpty()) {
+      hosts = client.listHosts(pattern).get();
+    } else if (pattern.isEmpty() && !labels.isEmpty()) {
+      hosts = client.listHosts(labels).get();
+    } else {
+      hosts = client.listHosts(pattern, labels).get();
+    }
 
     final Map<String, String> queryParams = Maps.newHashMap();
     final String statusFilter = options.getString(statusArg.getDest());
@@ -154,13 +159,6 @@ public class HostListCommand extends ControlCommand {
     }
 
     final List<String> sortedHosts = natural().sortedCopy(hosts);
-
-    final List<HostSelector> hostSelectors = Utils.parseHostSelectors(options, hostSelectorsArg);
-
-    if (hostSelectors != null && !hostSelectors.isEmpty() && json) {
-      System.err.println("Warning: host selectors are not supported for JSON output. Not applying"
-                         + " any host selectors.");
-    }
 
     if (quiet) {
       if (json) {
@@ -199,21 +197,6 @@ public class HostListCommand extends ControlCommand {
           final HostStatus s = e.getValue().get();
 
           if (s == null) {
-            continue;
-          }
-
-          boolean skipHost = false;
-          if (hostSelectors != null && !hostSelectors.isEmpty()) {
-            final Map<String, String> hostLabels = s.getLabels();
-            for (final HostSelector selector : hostSelectors) {
-              if (!hostLabels.containsKey(selector.getLabel()) ||
-                  !selector.matches(hostLabels.get(selector.getLabel()))) {
-                skipHost = true;
-                break;
-              }
-            }
-          }
-          if (skipHost) {
             continue;
           }
 
@@ -267,11 +250,11 @@ public class HostListCommand extends ControlCommand {
             }
           }
 
-          final String labels = Joiner.on(", ").withKeyValueSeparator("=").join(s.getLabels());
+          final String hostLabels = Joiner.on(", ").withKeyValueSeparator("=").join(s.getLabels());
 
           table.row(formatHostname(full, host), status, s.getJobs().size(),
               runningDeployedJobs.size(), cpus, mem, loadAvg, memUsage, os, version, docker,
-              labels);
+              hostLabels);
         }
 
         table.print();
