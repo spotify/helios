538337c5b5b8387e80b3dd234fc2fd592a2f5ad5
David Xia <dxia@spotify.com>
2017-01-10 13:35:15 -0500

    Add tests for JobCreateCommand and JobInspectCommand

--- helios-system-tests/src/main/java/com/spotify/helios/system/JobCreateTest.java
+++ helios-system-tests/src/main/java/com/spotify/helios/system/JobCreateTest.java
@@ -25,10 +25,19 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertThat;
 
 import com.spotify.helios.common.descriptors.Job;
+import com.spotify.helios.common.descriptors.PortMapping;
+
+import java.util.Collections;
+
 import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class JobCreateTest extends SystemTestBase {
+
+  @Rule
+  public final ExpectedException expectedException = ExpectedException.none();
 
   @Test
   public void testInvalidHostname() throws Exception {
@@ -44,5 +53,46 @@ public class JobCreateTest extends SystemTestBase {
                                                  .setHostname("$%^&")
                                                  .build());
     assertThat(output, Matchers.containsString("Invalid hostname "));
+  }
+
+  @Test
+  public void testPortMappingWithIp() throws Exception {
+    startDefaultMaster();
+    startDefaultAgent(testHost());
+    awaitHostStatus(testHost(), UP, LONG_WAIT_SECONDS, SECONDS);
+
+    // Create job with a portmapping that has an IP. Check it doesn't throw.
+    createJobRawOutput(Job.newBuilder()
+        .setName(testJobName)
+        .setVersion(testJobVersion)
+        .setImage(BUSYBOX)
+        .setPorts(Collections.singletonMap("foo", PortMapping.builder()
+            .ip("127.0.0.1")
+            .internalPort(80)
+            .externalPort(80)
+            .build()
+        )).build());
+  }
+
+  @Test
+  public void testPortMappingWithBadIp() throws Exception {
+    startDefaultMaster();
+    startDefaultAgent(testHost());
+    awaitHostStatus(testHost(), UP, LONG_WAIT_SECONDS, SECONDS);
+
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("foobar is not a valid IP address.");
+
+    // Create job with a portmapping that has a bad IP. Check it throws.
+    createJobRawOutput(Job.newBuilder()
+        .setName(testJobName)
+        .setVersion(testJobVersion)
+        .setImage(BUSYBOX)
+        .setPorts(Collections.singletonMap("foo", PortMapping.builder()
+            .ip("foobar")
+            .internalPort(80)
+            .externalPort(80)
+            .build()
+        )).build());
   }
 }
