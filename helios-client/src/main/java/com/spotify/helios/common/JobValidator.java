66491a5b5940492b75cb40c5377a831477f11b13
rwstephenson <rob@rwstephenson.com>
2017-01-11 13:43:48 +0100

    Fix namespace/repo pattern checking inversion

--- helios-client/src/main/java/com/spotify/helios/common/JobValidator.java
+++ helios-client/src/main/java/com/spotify/helios/common/JobValidator.java
@@ -436,14 +436,14 @@ public class JobValidator {
       repo = nameParts[0];
       name = nameParts[1];
     }
-    if (!NAMESPACE_PATTERN.matcher(repo).matches()) {
-      errors.add(
-          format("Invalid namespace name (%s), only [a-z0-9_] are allowed, size between 4 and 30",
-                 repo));
+    if (!REPO_PATTERN.matcher(repo).matches()) {
+      errors.add(format("Invalid repository name (%s), only [a-z0-9-_.] are allowed", repo));
       valid = false;
     }
-    if (!REPO_PATTERN.matcher(name).matches()) {
-      errors.add(format("Invalid repository name (%s), only [a-z0-9-_.] are allowed", name));
+    if (!NAMESPACE_PATTERN.matcher(name).matches()) {
+      errors.add(
+        format("Invalid namespace name (%s), only [a-z0-9_] are allowed, size between 4 and 30",
+               name));
       valid = false;
     }
     return valid;
