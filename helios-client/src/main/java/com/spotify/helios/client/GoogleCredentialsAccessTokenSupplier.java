bb032a505448b9cc64029b0da6b5eeb1ee60aeac
Matt Brown <mattbrown@spotify.com>
2018-03-19 16:08:32 -0400

    auth: throw exception if file at HELIOS_GOOGLE_CREDENTIALS cannot be read
    This changes the behavior when the HELIOS_GOOGLE_CREDENTIALS environment
    variable is set to a file that does not exist or cannot be read from
    falling to Application Default Credentials to instead throw an
    exception.
    
    If the environment variable contains invalid configuration, it seems
    better to throw an exception rather than silently use a different set of
    (possibly incorrect) credentials.
    
    As a part of this, I moved the GoogleCredential-loading logic into
    HeliosClient.Builder to simplify GoogleCredentialsAccessTokenSupplier
    even further.

--- helios-client/src/main/java/com/spotify/helios/client/GoogleCredentialsAccessTokenSupplier.java
+++ helios-client/src/main/java/com/spotify/helios/client/GoogleCredentialsAccessTokenSupplier.java
@@ -1,35 +1,29 @@
 /*
  *  -\-\-
- *  helios-client
- *  --
- *  Copyright (C) 2017 Spotify AB
- *  --
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * helios-client
+ * --
+ * Copyright (C) 2017 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  -/-/-
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
  *
  */
 
 package com.spotify.helios.client;
 
-import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,96 +32,21 @@ class GoogleCredentialsAccessTokenSupplier implements AuthorizationHeaderSupplie
   private static final Logger LOG =
       LoggerFactory.getLogger(GoogleCredentialsAccessTokenSupplier.class);
 
-  public static final List<String> DEFAULT_SCOPES = ImmutableList.of(
-      "https://www.googleapis.com/auth/cloud-platform.read-only",
-      "https://www.googleapis.com/auth/userinfo.email"
-  );
+  private final GoogleCredentials credentials;
 
-  private final Optional<AccessToken> staticToken;
-  private final List<String> tokenScopes;
-  private final Object lock = new Object();
-  private GoogleCredentials credentials;
-
-  GoogleCredentialsAccessTokenSupplier() {
-    this(null, DEFAULT_SCOPES);
-  }
-
-  GoogleCredentialsAccessTokenSupplier(final AccessToken staticToken) {
-    this(staticToken, DEFAULT_SCOPES);
-  }
-
-  GoogleCredentialsAccessTokenSupplier(final AccessToken staticToken,
-                                       final List<String> tokenScopes) {
-    this(staticToken, tokenScopes, null);
-  }
-
-  @VisibleForTesting
-  GoogleCredentialsAccessTokenSupplier(final AccessToken staticToken,
-                                       final List<String> tokenScopes,
-                                       final GoogleCredentials credentials) {
-    this.staticToken = Optional.fromNullable(staticToken);
-    this.tokenScopes = tokenScopes;
+  GoogleCredentialsAccessTokenSupplier(final GoogleCredentials credentials) {
     this.credentials = credentials;
   }
 
   @Override
   public Optional<String> get() {
-    if (staticToken.isPresent()) {
-      return Optional.of("Bearer " + staticToken.get().getTokenValue());
-    }
-
     try {
-      synchronized (lock) {
-        if (credentials == null) {
-          credentials = getCredentialsWithScopes(tokenScopes);
-        }
-        // call getRequestMetadata to kick the if-expired-then-renew check
-        credentials.getRequestMetadata(null);
-      }
-
+      // call getRequestMetadata to kick the if-expired-then-renew check
+      credentials.getRequestMetadata(null);
       return Optional.of("Bearer " + credentials.getAccessToken().getTokenValue());
     } catch (IOException | RuntimeException e) {
-      LOG.debug("Exception (possibly benign) while loading Google Credentials", e);
+      LOG.error("Exception while refreshing Google Credentials", e);
       return Optional.absent();
     }
-  }
-
-  /**
-   * Attempt to load Google Credentials with specified scopes.
-   * <ol>
-   * <li>First check to see if the environment variable HELIOS_GOOGLE_CREDENTIALS is set
-   * and points to a readable file</li>
-   * <li>Otherwise check if Google Application Default Credentials (ADC) can be loaded</li>
-   * </ol>
-   *
-   * <p>Note that we use a special environment variable of our own in addition to any environment
-   * variable that the ADC loading uses (GOOGLE_APPLICATION_CREDENTIALS) in case there is a need
-   * for the user to use the latter env var for some other purpose.
-   *
-   * @return Return a {@link GoogleCredentials}
-   */
-  private static GoogleCredentials getCredentialsWithScopes(final List<String> scopes)
-      throws IOException {
-    GoogleCredentials credentials = null;
-
-    // first check whether the environment variable is set
-    final String googleCredentialsPath = System.getenv("HELIOS_GOOGLE_CREDENTIALS");
-    if (googleCredentialsPath != null) {
-      final File file = new File(googleCredentialsPath);
-      if (file.exists()) {
-        try (final FileInputStream s = new FileInputStream(file)) {
-          credentials = GoogleCredentials.fromStream(s);
-          LOG.info("Using Google Credentials from file: " + file.getAbsolutePath());
-        }
-      }
-    }
-
-    // fallback to application default credentials
-    if (credentials == null) {
-      credentials = GoogleCredentials.getApplicationDefault();
-      LOG.info("Using Google Application Default Credentials");
-    }
-
-    return scopes.isEmpty() ? credentials : credentials.createScoped(scopes);
   }
 }
--- helios-client/src/test/java/com/spotify/helios/client/GoogleCredentialsAccessTokenSupplierTest.java
+++ helios-client/src/test/java/com/spotify/helios/client/GoogleCredentialsAccessTokenSupplierTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,37 +26,19 @@ import static org.hamcrest.Matchers.equalTo;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.base.Optional;
-import java.io.IOException;
 import org.junit.Test;
 
 public class GoogleCredentialsAccessTokenSupplierTest {
 
-  @Test
-  public void testGetWhenDisabled() {
-    final GoogleCredentialsAccessTokenSupplier supplier =
-        new GoogleCredentialsAccessTokenSupplier(null, null);
-    assertThat(supplier.get(), equalTo(Optional.<String>absent()));
-  }
+  private final AccessToken accessToken = new AccessToken("foobar", null);
+
+  // instantiating the GoogleCredentials class directly is a big hack to workaround the fact that
+  // we cannot stub the final method getAccessToken()
+  private final GoogleCredentialsAccessTokenSupplier supplier =
+      new GoogleCredentialsAccessTokenSupplier(new GoogleCredentials(accessToken));
 
   @Test
-  public void testGetWithStaticToken() {
-    final AccessToken token = new AccessToken("token", null);
-    final GoogleCredentialsAccessTokenSupplier supplier =
-        new GoogleCredentialsAccessTokenSupplier(token, null);
-    assertThat(supplier.get(), equalTo(Optional.of("Bearer token")));
-  }
-
-  @Test
-  public void testGetWithCredentials() throws IOException {
-    final AccessToken accessToken = new AccessToken("foobar", null);
-
-    // instantiating the GoogleCredentials class directly is a big hack to workaround the fact that
-    // we cannot stub the final method getAccessToken()
-    final GoogleCredentials credentials = new GoogleCredentials(accessToken);
-
-    final GoogleCredentialsAccessTokenSupplier supplier = new GoogleCredentialsAccessTokenSupplier(
-        null, null, credentials);
-
+  public void testGetWithCredentials() {
     assertThat(supplier.get(), equalTo(Optional.of("Bearer foobar")));
   }
 }
